d910f30bd4ccaf77b2b45ce9a1899dad3baabfd7
william881218 <william881218@gmail.com>
2020-03-21 15:58:45 +0800

    text tokenized & indexed & lowerCased, ArticleDataset Implemented

--- data.py
+++ data.py
@@ -1,41 +1,80 @@
 import torch
 import json
 import nltk
+import re
 
 #word2vec_lines = open('./glove.6B.50d.txt').read().strip().split('\n')
 #for line in word2vec_lines:
 
 class Article():
-        def __init__(self, id, tokened_text, extractive_gt, abstractive_gt):
-            self.id = id
-            self.tokened_text = tokened_text
-            self.extractive_gt = extractive_gt
-            self.abstractive_gt = abstractive_gt
-        def show(self):
-            print('id: {}'.format(self.id))
-            print('tokened_text:')
-            for sentence in self.tokened_text: print(sentence, end='')
-            print('extractive_gt: {}'.format(self.extractive_gt))
-            print('abstractive_gt: {}'.format(self.abstractive_gt))
+    def __init__(self, article, word2index, index2vec):
+        self.id = article['id']
+        self.sentence_cut = []
+        self.tokened_text = []
+        self.text_size = 0
+        for line in article['text']:
+            self.sentence_cut.append((self.text_size, self.text_size + len(line)))
+            self.text_size += len(line)
+            for word in line:
+                self.tokened_text.append(word2index[word])
+                #self.tokened_text.append(word)
+        self.extractive_gt = article['extractive_summary']
+        #self.abstractive_gt = article['summary']
+        self.abstractive_gt = [word2index[word] for word in article['summary']]
+    def show(self):
+        print('id: {}, text_size: {}'.format(self.id, self.text_size))
+        print('tokened_text:')
+        for word in self.tokened_text:
+            print('{}, '.format(word), end='')
+        print('')
+        for i, cut in enumerate(self.sentence_cut):
+            print('Sentence {}: {}'.format(i, self.tokened_text[cut[0]: cut[1]]))
+        print('extractive_gt: {}'.format(self.extractive_gt))
+        print('abstractive_gt: {}'.format(self.abstractive_gt))
+
+"""
+Json file keys:
+id, text, summary, sent_bounds, extractive_summary
+"""
+
+def read_glove():
+    glove = open('glove.6B.50d.txt').read().split('\n')
+    glove = [line.split(' ') for line in glove]
+    return {line[0]:line[1:] for line in glove}
+
+class ArticleDataset():
+    def __init__(self, dataset_path):
+        lines = open(dataset_path).read().lower().strip().split('\n')
+        datas = [json.loads(line) for line in lines]
+        self.articles = []
+        self.word2index = {}
+        self.index2vec = []
+        glove = read_glove()
+        for data in datas:
+            data['text'] = [nltk.word_tokenize(data['text'][begin:end]) for begin, end in data['sent_bounds'] ]
+            data['summary'] = [word for word in nltk.word_tokenize(data['summary'])]
+            for line in data['text']:
+                for word in line:
+                    if word not in self.word2index:
+                        self.word2index[word] = len(self.index2vec)
+                        self.index2vec.append(glove[word])
+            for word in data['summary']:
+                if word not in self.word2index:
+                    self.word2index[word] = len(self.index2vec)
+                    self.index2vec.append(glove[word])
+            self.articles.append(Article(data, self.word2index, self.index2vec))
+        pass
+    def __len__(self):
+        return len(self.articles)
+    def __getitem__(self, idx):
+        return self.articles[int(idx)]
 
 def text_to_token(data):
     article = data['text']
     article = [article[int(start):int(end)] for start, end in data['sent_bounds']]
     return [nltk.word_tokenize(sentence) for sentence in article]
 
-# Read a file and split into lines
-def readLines(filename):
-    lines = open(filename).read().strip().split('\n')
-    return [json.loads(line) for line in lines]
 
-# Read datas from file into three list: tokened_text, extractive_ground_truth and abstractive_ground_truth
-def readDatas(filename):
-    datas = readLines(filename)
-    articles = []
-    for data in datas:
-        tokened_text = text_to_token(data)
-        articles.append(Article(data['id'], tokened_text, data['extractive_summary'], nltk.word_tokenize(data['summary'])))
-    return articles
 
-valid_datas = readDatas('./valid.jsonl')
-valid_datas[0].show()
+valid_dataset = ArticleDataset('./short.jsonl')
+valid_dataset[0].show()

89b4d9f4dfcc8cdf8d15f7fc8789dd6c83ff18e7
william881218 <william881218@gmail.com>
2020-03-27 01:29:21 +0800

    Add sigmoid in linear layer

--- extractive/model.py
+++ extractive/model.py
@@ -8,6 +8,7 @@ class WordEmbedding(nn.Module):
         super(WordEmbedding, self).__init__()
         self.embedding_layer = nn.Embedding(vocab_size, embedding_dim)
         self.embedding_layer.weight.data.copy_(torch.from_numpy(matrix.astype('float32')))
+        
     def forward(self, input):
         with torch.no_grad():
             output = self.embedding_layer(input)
@@ -15,23 +16,30 @@ class WordEmbedding(nn.Module):
 
 class RNNTagger(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, output_size):
+
         super(RNNTagger, self).__init__()
         self.hidden_dim = hidden_dim
+
         # The GRU takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
         self.rnn = nn.GRU(embedding_dim, hidden_dim, batch_first=True, bidirectional=True)
+
         # The linear layer that maps from hidden state space to tag space
         self.hidden2tag_1 = nn.Linear(hidden_dim * 2, 64)
         self.sigmoid = nn.Sigmoid()
         self.hidden2tag_2 = nn.Linear(64, output_size)
+
     def forward(self, packedSequence):
-        #feed into the rnn model, get the output
-        #output = model(x_packed)
+
+        #feed into the rnn model
         output_packed, _ = self.rnn(packedSequence)
+
+        #pad the output in order to fit into linear layers
         output_padded, output_lengths = pad_packed_sequence(output_packed, batch_first=True)
-        #print('output_padded: ~~~~~~``')
-        #print(output_padded)
+
+        #Feed into Linear layers
         output_padded = self.hidden2tag_1(output_padded)
         output_padded = self.sigmoid(output_padded)
         output_padded = self.hidden2tag_2(output_padded)
+
         return output_padded, output_lengths

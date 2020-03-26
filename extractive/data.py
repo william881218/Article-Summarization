c010eab82343dc995834dd5fcd7bace1b8c5a0ab
william881218 <william881218@gmail.com>
2020-03-27 01:19:15 +0800

    Add comment, typo fixed

--- extractive/data.py
+++ extractive/data.py
@@ -13,14 +13,17 @@ GPU_DEVICE = 1
 word_vec_d = 50
 glove_path = './glove.6B.50d.txt'
 softmax = nn.Softmax(dim=1)
+prediction_softmax = nn.Softmax(dim=0)
 if torch.cuda.is_available():
     softmax = softmax.cuda(GPU_DEVICE)
+    prediction_softmax = prediction_softmax.cuda(GPU_DEVICE)
 
-
+#Read in word embedding
 def read_glove():
     glove = open(glove_path).read().split('\n')
     glove = [line.split(' ') for line in glove]
     return {line[0]:line[1:] for line in glove}
+
 #read glove only once
 glove = read_glove()
 print('glove read complete')
@@ -30,7 +33,7 @@ class Article():
         self.id = article['id']
         self.index = int(idx)
         self.sentence_cut = []
-        self.tokened_text = np.ndarray(shape=(1,0))
+        self.tokened_text = []
         self.token_num = 0
         self.extractive_gt = None if predicting else article['extractive_summary']
         self.abstractive_gt = None if predicting else [word2index[word] for word in article['summary']]
@@ -40,7 +43,7 @@ class Article():
             self.sentence_cut.append((self.token_num, self.token_num + len(line)))
             self.token_num += len(line)
             for word in line:
-                self.tokened_text = np.append(self.tokened_text, [word2index[word]])
+                self.tokened_text.append(word2index[word])
 
     def show(self):
         print('id: {}, index: {}, token_num: {}'.format(self.id, self.index, self.token_num))
@@ -58,10 +61,9 @@ Json file keys:
 id, text, summary, sent_bounds, extractive_summary
 """
 
-
-
 class ArticleDataset(Dataset):
     def __init__(self, dataset_path, predicting=False):
+
         #read in data into a list of dict
         lines = open(dataset_path).read().lower().strip().split('\n')
         datas = [json.loads(line) for line in lines]
@@ -69,6 +71,7 @@ class ArticleDataset(Dataset):
         #initialize
         self.articles = []
         self.word2index = {}
+
         #prepare word embedding: 0th index -> 0-vec
         self.index2vec = [[0 for _ in range(word_vec_d)]]
         self.positive_tag = 0
@@ -98,7 +101,8 @@ class ArticleDataset(Dataset):
                         try:
                             self.index2vec += [glove[word]]
                         except KeyError: #if we can't find the word in glove, add a random vector as decode
-                            self.index2vec += [np.random.standard_normal(word_vec_d).tolist()]
+                            self.index2vec += [np.zeros(word_vec_d).tolist()]
+
             #the same procedure on abstractive_gt
             if not predicting:
                 #expending word2index dict
@@ -108,7 +112,7 @@ class ArticleDataset(Dataset):
                         try:
                             self.index2vec += [glove[word]]
                         except KeyError:
-                            self.index2vec += [np.random.standard_normal(word_vec_d).tolist()]
+                            self.index2vec += [np.zeros(word_vec_d).tolist()]
 
             #use this data to create Article class
             self.articles.append(Article(data, self.word2index, predicting=predicting, idx=len(self.articles)))
@@ -131,33 +135,51 @@ class ArticleDataset(Dataset):
         #return the i-th article's text and extractive_gt in order to train the model
         article = self.articles[int(idx)]
 
-        if article.extractive_gt == None: #if we're predicting, we haven no extractive_gt
-            return torch.from_numpy(article.tokened_text.reshape([article.token_num]).astype(dtype='int32')), idx
+        #if we're predicting, we haven no extractive_gt
+        if article.extractive_gt == None:
+            return torch.tensor(article.tokened_text).int(), idx
 
         #label tokens in extractive_gt as True, while other tokens in text to be False
         extractive_gt = np.zeros(shape=(article.token_num))
         ext_cut = article.sentence_cut[int(article.extractive_gt)]
         extractive_gt[ext_cut[0]:ext_cut[1]] = 1.
 
-        return (torch.from_numpy(article.tokened_text.reshape([article.token_num]).astype(dtype='int32')),
-                torch.from_numpy(extractive_gt), idx)
+        return (torch.tensor(article.tokened_text).int(), torch.from_numpy(extractive_gt), idx)
+
+    def show(self):
+        print('word2index:\n----------------')
+        print(self.word2index)
+        #prepare word embedding: 0th index -> 0-vec
+        print('index2vec:\n----------------')
+        print(np.shape(self.index2vec))
+        print('positive_tag: {}, total_tag: {}'.format(self.positive_tag, self.total_tag))
+        print('Articles:')
+        for article in self.articles:
+            article.show()
 
     def word_embedding(self):
         return self.index2vec
 
-    def article(self, idx):
-        return self.articles[int(idx)]
+    def article(self, article_idx):
+        return self.articles[int(article_idx)]
 
+    #Given probability distribution of sent_idx_th article, predict the extractive ground truth
     def predict(self, sent_idx, output):
-        article = self.articles[sent_idx]
-        #print(output.shape)
-        #print(article.token_num)
+
+        #Applied Softmax Layer with GPU acceleration
         if torch.cuda.is_available():
             output = output.cuda(GPU_DEVICE)
-        output = softmax(output)
-        sent_candidates = []
+        output = prediction_softmax(output)
+
+        #find the given article
+        article = self.articles[sent_idx]
         if len(article.sentence_cut) == 0:
             return 0
+
+        #Used to store each sentence's average probability
+        sent_candidates = []
+
+        #Calculate each sentence's average probability of becoming tagged
         for i_sent, (start, end) in enumerate(article.sentence_cut):
             sum = 0.
             for token in range(start, end):
@@ -166,6 +188,5 @@ class ArticleDataset(Dataset):
                 sent_candidates.append((i_sent, sum / (end - start)))
             except:
                 pass
-        sent_candidates.sort(key=lambda x: x[1], reverse=True)
-        #print(sent_candidates)
-        return sent_candidates[0][0]
+
+        return max(sent_candidates, key=lambda x: x[1])[0]

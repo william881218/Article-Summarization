7275aad2076eb06d87ed09da54e7bce80537a471
william881218 <william881218@gmail.com>
2020-03-27 17:29:54 +0800

    Command Line tool used

--- extractive/train.py
+++ extractive/train.py
@@ -8,18 +8,18 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.nn import BCEWithLogitsLoss
 from torch.nn.functional import one_hot
 
-batch_size = 10
-valid_batch_size = 20
-training_data_path = './train_short.jsonl'
-validation_data_path = './train_short.jsonl'
+TRAINING_DATASET_PATH = './train.jsonl'
+VALIDATION_DATASET_PATH = './valid.jsonl'
+batch_size = 16
+validation_batch_size = 50
 learning_rate = 0.1
+using_adam = True
+adam_learning_rate = 0.01
 SGD_momentum = 0.8
 rnn_hidden_dim=64
 tag_type_num=1
-ipoch_num = 100
+ipoch_num = 50
 all_losses = []
-
-
 
 def pad_collate(batch):
     #sort the batch according to sentence's length (requested by padding)
@@ -59,7 +59,7 @@ def validate(rnn, valid_dataLoader_it, validation_embedding, validation_dataset)
             predicted_gt = validation_dataset.predict(idx[i_sent], output[i_sent, 0:sent_len])
             acc_count += (predicted_gt == extractive_gt)
 
-        return acc_count / valid_batch_size, len(idx)
+        return acc_count / len(idx), len(idx)
 
 #Used to generate validation DataLoader iterator
 def cycle(iterable):
@@ -67,24 +67,28 @@ def cycle(iterable):
         for x in iterable:
             yield x
 
-def main():
+def main(training_dataset_path=TRAINING_DATASET_PATH, validation_dataset_path=VALIDATION_DATASET_PATH):
+
     #prepare training dataset with DataLoader, which will return a batched, padded sample
-    training_dataset = ArticleDataset(training_data_path)
+    training_dataset = ArticleDataset(training_dataset_path)
     training_data_loader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True, collate_fn=pad_collate)
 
     #prepare validation dataset
-    validation_dataset = ArticleDataset(validation_data_path)
-    valid_data_loader = DataLoader(validation_dataset, batch_size = valid_batch_size, shuffle=True, collate_fn=pad_collate)
+    validation_dataset = ArticleDataset(validation_dataset_path)
+    valid_data_loader = DataLoader(validation_dataset, batch_size = validation_batch_size, shuffle=True, collate_fn=pad_collate)
     valid_dataLoader_it = iter(cycle(valid_data_loader))
 
     #prepare embedding layer
     trainingSet_embedding = WordEmbedding(training_dataset.vocab_size, word_vec_d, training_dataset.index2vec)
     validation_embedding = WordEmbedding(validation_dataset.vocab_size, word_vec_d, validation_dataset.index2vec)
 
-    #creating model and optimizer
+    #create model
     rnn = RNNTagger(embedding_dim=word_vec_d, hidden_dim=rnn_hidden_dim, output_size=tag_type_num)
-    #optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate, momentum=SGD_momentum)
-    optimizer = torch.optim.Adam(rnn.parameters(), lr=0.001)
+
+    #Choose optimizer
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate, momentum=SGD_momentum)
+    if using_adam:
+        optimizer = torch.optim.Adam(rnn.parameters(), lr=0.001)
 
     #use pos_weight in BCELoss to handle data imbalance
     pos_tag = training_dataset.positive_tag
@@ -117,7 +121,6 @@ def main():
             output = output.view(output.shape[0], -1)
             y_padded = y_padded.type_as(output)
 
-
             #evaluate start
             loss_sum = 0
             #calculate loss acording to each sentence's length
@@ -128,15 +131,23 @@ def main():
                 loss.backward(retain_graph=True)
                 optimizer.step()
 
-            #record the losses and print the info
+            #record the losses and print the info every 10 batch
             all_losses.append(loss_sum)
-            if i_batch % 1 == 0:
+            if i_batch % 10 == 0:
                 valid_acc, valid_num = validate(rnn, valid_dataLoader_it, validation_embedding, validation_dataset)
-                print('ipoch [{}/{}], step {}, current loss: {}, size {}, valid_acc: {}, size: {}'.format(i_ipoch+1, ipoch_num, i_batch, all_losses[-1], len(output_lengths), valid_acc, valid_num))
+                print('ipoch [{}/{}], step [{}/{}], current loss: {}, size {}, valid_acc: {}, size: {}'.format(i_ipoch+1,
+                      ipoch_num, i_batch + 1, len(training_dataset) // batch_size + 1, all_losses[-1], len(output_lengths), valid_acc, valid_num))
 
     #training end, save the model
-    torch.save(rnn, 'ipoch={}_lr={}_data={}.pt'.format(ipoch_num, learning_rate, training_data_path[2:]))
+    torch.save(rnn, 'ipoch={}_lr={}_train={}_opt={}.pt'.format(ipoch_num, learning_rate, training_dataset_path[2:], 'Adam' if using_adam else 'SGD'))
     return
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print('usage: python train.py [training dataset path] [validation dataset path]')
+        print('if path not assigned, training dataset: {}, validation dataset: {}'.format(TRAINING_DATASET_PATH, VALIDATION_validation_batch_size))
+        exit()

5d5fb08c1a5f8437c6879dedbe7320306507b2a0
william881218 <william881218@gmail.com>
2020-04-01 23:23:29 +0800

    Predict successfully

--- abstractive/predict.py
+++ abstractive/predict.py
@@ -13,51 +13,54 @@ def pad_collate_predicting(batch):
     batch.sort(key=lambda x: len(x[0]), reverse=True)
     x_data, idx = zip(*batch)
     x_len = [len(x) for x in x_data]
-    x_padded = pad_sequence(x_data, batch_first=True, padding_value=0)
+    x_padded = pad_sequence(x_data, batch_first=True, padding_value=PAD_ID)
     return x_padded, x_len, idx
+
+def evaluate(seq2seq, batch):
+    with torch.no_grad():
+        x_padded, x_len, idx = batch
+        if torch.cuda.is_available():
+            x_padded = x_padded.cuda(GPU_DEVICE)
+
+        decoder_output, hidden = seq2seq.predict((x_padded, x_len))
+
+        topv, topi = torch.topk(decoder_output, 1)
+        outputs = topi.squeeze().transpose(0, 1).contiguous()
+
+        return outputs, idx
 
 def main(model_path=MODEL_PATH, input_file_path=INPUT_FILE_PATH, output_path=OUTPUT_PATH):
 
     #create predicting dataset and dataloader
     predicting_dataset = ArticleDataset(input_file_path, predicting=True)
-    article_data_loader = DataLoader(predicting_dataset, batch_size=predict_batch_size, shuffle=False, collate_fn=pad_collate_predicting)
+    predicting_data_loader = DataLoader(predicting_dataset, batch_size=predict_batch_size,
+                                            shuffle=False, collate_fn=pad_collate_predicting)
 
-    #prepare embedding model and predicting model
-    embedding = WordEmbedding(predicting_dataset.vocab_size, word_vec_d, predicting_dataset.index2vec)
-    rnn = torch.load(model_path, map_location=torch.device('cpu'))
+    #load the model
+    seq2seq = torch.load(model_path, map_location=torch.device('cpu'))
 
     #if gpu is available
     if torch.cuda.is_available():
-        rnn = rnn.cuda()
-        embedding = embedding.cuda()
+        seq2seq = seq2seq.cuda(GPU_DEVICE)
 
     output_list = []
-    for i_batch, (x_padded, x_len, idx) in enumerate(article_data_loader):
+
+    for i_batch, batch in enumerate(predicting_data_loader):
 
         #no need to backprop, so no_grad
-        with torch.no_grad():
+        outputs, idx = evaluate(seq2seq, batch)
 
-            x_padded = x_padded.long()
-            if torch.cuda.is_available():
-                x_padded = x_padded.cuda()
-
-            #Convert word to vector
-            x_embed = embedding(x_padded)
-
-            #packing
-            x_packed = pack_padded_sequence(x_embed, x_len, batch_first=True, enforce_sorted=False)
-
-            #Feeding into rnn and get an output
-            output, output_lengths = rnn(x_packed)
-
-            for i_sent, sent_len in enumerate(output_lengths):
-                predicted_gt = predicting_dataset.predict(idx[i_sent], output[i_sent, 0:sent_len])
-                output_list.append('{' + '"id": "{}", "predict_sentence_index": [{}]'.format(predicting_dataset.article(idx[i_sent]).id, predicted_gt) + '}\n')
-                print({'{}_th prediction completed.'.format(i_sent)})
+        for i, output in enumerate(outputs):
+            output_str = []
+            for word in output:
+                if word.item() == EOS_ID:
+                    break
+                output_str += [seq2seq.index2word[word.item()]]
+            output_list += ['\"id\": \"{}\", \"predict\": \"{}\"'.format(idx[i], ' '.join(output_str))]
 
     with open(output_path, 'w') as output_file:
         for output in output_list:
-            output_file.write(output)
+            output_file.write('{' + output + '}\n')
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:

5c2bce96f126e6caaf0b90f6779d79134732aa5c
william881218 <william881218@gmail.com>
2020-04-03 23:11:39 +0800

    Use less GPU resources

--- abstractive/predict.py
+++ abstractive/predict.py
@@ -7,7 +7,9 @@ import json
 INPUT_FILE_PATH = './test_short.jsonl'
 OUTPUT_PATH = './output.jsonl'
 MODEL_PATH = './best.pt'
-predict_batch_size = 2
+INDEX2WORD_PATH = './model/index2word.pt'
+INDEX2VEC_PATH = './model/index2vec.pt'
+predict_batch_size = 5
 
 def pad_collate_predicting(batch):
     batch.sort(key=lambda x: len(x[0]), reverse=True)
@@ -16,13 +18,12 @@ def pad_collate_predicting(batch):
     x_padded = pad_sequence(x_data, batch_first=True, padding_value=PAD_ID)
     return x_padded, x_len, idx
 
-def evaluate(seq2seq, batch):
+def evaluate(seq2seq, batch, index2vec):
     with torch.no_grad():
         x_padded, x_len, idx = batch
-        if torch.cuda.is_available():
-            x_padded = x_padded.cuda(GPU_DEVICE)
+        x_padded.to(DEVICE)
 
-        decoder_output, hidden = seq2seq.predict((x_padded, x_len))
+        decoder_output, hidden = seq2seq.predict((x_padded, x_len), index2vec)
 
         topv, topi = torch.topk(decoder_output, 1)
         outputs = topi.squeeze().transpose(0, 1).contiguous()
@@ -38,25 +39,29 @@ def main(model_path=MODEL_PATH, input_file_path=INPUT_FILE_PATH, output_path=OUT
 
     #load the model
     seq2seq = torch.load(model_path, map_location=torch.device('cpu'))
+    index2word = torch.load(INDEX2WORD_PATH, map_location=torch.device('cpu'))
+    index2vec = WordEmbedding(vocab_size=predicting_dataset.vocab_size,
+                              embedding_dim=word_vec_d, matrix=predicting_dataset.index2vec)
 
     #if gpu is available
-    if torch.cuda.is_available():
-        seq2seq = seq2seq.cuda(GPU_DEVICE)
+    seq2seq = seq2seq.to(DEVICE)
+
+
 
     output_list = []
 
     for i_batch, batch in enumerate(predicting_data_loader):
 
         #no need to backprop, so no_grad
-        outputs, idx = evaluate(seq2seq, batch)
-
+        outputs, idx = evaluate(seq2seq, batch, index2vec)
         for i, output in enumerate(outputs):
             output_str = []
             for word in output:
                 if word.item() == EOS_ID:
                     break
-                output_str += [seq2seq.index2word[word.item()]]
+                output_str += [index2word.index2word[word.item()]]
             output_list += ['\"id\": \"{}\", \"predict\": \"{}\"'.format(idx[i], ' '.join(output_str))]
+        print('batch {} complete'.format(i_batch))
 
     with open(output_path, 'w') as output_file:
         for output in output_list:
--- abstractive/train.py
+++ abstractive/train.py
@@ -11,14 +11,14 @@ from torch.nn.functional import one_hot
 TRAINING_DATASET_PATH = '../valid_short.jsonl'
 VALIDATION_DATASET_PATH = '../valid_short.jsonl'
 batch_size = 10
-validation_batch_size = 3
+validation_batch_size = 20
 learning_rate = 0.1
 using_adam = True
 adam_learning_rate = 0.01
 SGD_momentum = 0.8
 rnn_hidden_dim=64
 tag_type_num=1
-ipoch_num = 20
+ipoch_num = 10
 max_output_len = 300
 teacher_forcing_ratio = 0.5
 all_losses = []
@@ -38,25 +38,16 @@ def pad_collate(batch):
     y_padded = pad_sequence(y_data, batch_first=True, padding_value=PAD_ID)
     return x_padded, y_padded.long(), x_len, y_len, list(idx)
 
-
-
-
 #Used to generate validation DataLoader iterator
 def cycle(iterable):
     while True:
         for x in iterable:
             yield x
 
-def train(seq2seq, batch, criterion):
+def train(seq2seq, batch, criterion, training_embedding):
     x_padded, y_padded, x_len, y_len, idx = batch
 
-    if torch.cuda.is_available():
-        x_padded = x_padded.cuda(GPU_DEVICE)
-        y_padded = y_padded.cuda(GPU_DEVICE)
-    x_padded = Variable(x_padded)
-    y_padded = Variable(y_padded)
-
-    decoder_output, hidden = seq2seq((x_padded, x_len), (y_padded, y_len))
+    decoder_output, hidden = seq2seq((x_padded, x_len), (y_padded, y_len), training_embedding)
 
     t, b = decoder_output.size(0), decoder_output.size(1)
     decoder_output = decoder_output.reshape(b * t, -1)
@@ -87,9 +78,11 @@ def main(training_dataset_path=TRAINING_DATASET_PATH, validation_dataset_path=VA
     decoder = Decoder(hidden_size=context_vector_dim,
                       output_size=training_dataset.vocab_size,
                       max_length=max_output_len,
-                      teacher_forcing_ratio=teacher_forcing_ratio,
-                      index2vec=training_embedding)
+                      teacher_forcing_ratio=teacher_forcing_ratio)
     seq2seq = Seq2Seq(encoder, decoder)
+    training_index2word = Index2word(training_dataset.index2word)
+    torch.save(training_index2word, '{}/index2word.pt'.format(model_path))
+    torch.save(training_embedding, '{}/index2vec.pt'.format(model_path))
 
     #Choose optimizer and criterion
     optimizer = torch.optim.SGD(seq2seq.parameters(), lr=learning_rate, momentum=SGD_momentum)
@@ -99,11 +92,13 @@ def main(training_dataset_path=TRAINING_DATASET_PATH, validation_dataset_path=VA
 
 
 
-    if torch.cuda.is_available():
-        seq2seq = seq2seq.cuda(GPU_DEVICE)
+
+    seq2seq.to(DEVICE)
         #trainingSet_embedding = trainingSet_embedding.cuda(GPU_DEVICE)
         #validation_embedding.cuda(GPU_DEVICE)
-        criterion.cuda(GPU_DEVICE)
+    criterion.to(DEVICE)
+    training_embedding.to(DEVICE)
+
 
 
     #training start
@@ -111,17 +106,20 @@ def main(training_dataset_path=TRAINING_DATASET_PATH, validation_dataset_path=VA
         for i_batch, batch in enumerate(training_data_loader):
 
             optimizer.zero_grad()
-            cur_loss = train(seq2seq, batch, criterion)
+            cur_loss = train(seq2seq, batch, criterion, training_embedding)
             cur_loss.backward()
             optimizer.step()
             all_losses.append(cur_loss.item())
 
+            
             #record the losses and print the info every 10 batch
-            if i_batch % 10 == 0:
-                valid_loss = train(seq2seq, next(valid_dataLoader_it), criterion)
-                print('ipoch [{}/{}], step [{}/{}], loss: {}, valid_loss: {}'.format(i_ipoch+1,
+            if i_batch % 5 == 0:
+                with torch.no_grad():
+                    valid_loss = train(seq2seq, next(valid_dataLoader_it), criterion, training_embedding)
+                    print('ipoch [{}/{}], step [{}/{}], loss: {}, valid_loss: {}'.format(i_ipoch+1,
                       ipoch_num, i_batch + 1, len(training_dataset) // batch_size + 1, all_losses[-1], valid_loss.item()))
-        torch.save(seq2seq, '{}/ipoch={}_ibatch={}_loss={}_valid={}.pt'.format(model_path, i_ipoch, i_batch, all_losses[-1], valid_loss.item()))
+                    if i_batch % 200 == 0:
+                        torch.save(seq2seq, '{}/ipoch={}_ibatch={}_loss={}_valid={}.pt'.format(model_path, i_ipoch, i_batch, all_losses[-1], valid_loss.item()))
 
         #print('ipoch {}, validates:'.format(i_ipoch))
 
